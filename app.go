2a8433a8d79dff02a1331fe3f87880d8d3e374f0
deathslayer5-dev <deathslayer532@gmail.com>
2026-02-09 20:40:46 -0500

    added git functionalities

--- app.go
+++ app.go
@@ -17,14 +17,24 @@ func main() {
 		os.Exit(1)
 	}
 	options := map[string]bool{
-		"help":    false,
-		"version": false,
-		"java":    false,
-		"project": false,
+		"help":      false,
+		"version":   false,
+		"java":      false,
+		"project":   false,
+		"git":       false,
+		"git-local": false,
 	}
 	name := ""
-
+	remote := ""
+	next := false
+	changed := false
 	for _, arg := range os.Args[1:] {
+		if next {
+			remote = arg
+			next = false
+			changed = true
+			continue
+		}
 		if arg == "--help" {
 			options["help"] = true
 			break
@@ -32,6 +42,11 @@ func main() {
 		if arg == "--version" {
 			options["version"] = true
 			break
+		}
+		if arg == "--git" {
+			options["git"] = true
+			next = true
+			continue
 		}
 		if !strings.HasPrefix(arg, "--") {
 			name = arg
@@ -42,8 +57,11 @@ func main() {
 			options[str] = true
 		}
 	}
+	if changed == false && next == true {
+		log.Fatal("Missing remote name")
+	}
 	if name == "" {
-		fmt.Println("Missing mkpro function")
+		log.Fatal("Missing mkpro function")
 	}
 	if options["help"] {
 		fmt.Println("Usage: mkpro \"--options\" <name>")
@@ -74,7 +92,15 @@ func main() {
 		}
 		fmt.Printf("Successfully created: %s\n", dir)
 
-		dir = fmt.Sprintf("%s%s", name, "/resources")
+		dir = fmt.Sprintf("%s%s%s", name, "src", "/resources")
+		cmd = exec.Command("mkdir", "-p", dir)
+		fmt.Printf("Creating directory: %s\n", dir)
+		_, err = cmd.Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Successfully created: %s\n", dir)
+		dir = fmt.Sprintf("%s%s%s", name, "src", "/main")
 		cmd = exec.Command("mkdir", "-p", dir)
 		fmt.Printf("Creating directory: %s\n", dir)
 		_, err = cmd.Output()
@@ -93,7 +119,16 @@ func main() {
 		}
 		fmt.Printf("Successfully created: %s\n", dir)
 
-		dir = fmt.Sprintf("%s%s", name, "/resources")
+		dir = fmt.Sprintf("%s%s%s", name, "/src", "/main")
+		cmd = exec.Command("mkdir", "-p", dir)
+		fmt.Printf("Creating directory: %s\n", dir)
+		_, err = cmd.Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Successfully created: %s\n", dir)
+
+		dir = fmt.Sprintf("%s%s%s", name, "/src", "/resources")
 		cmd = exec.Command("mkdir", "-p", dir)
 		fmt.Printf("Creating directory: %s\n", dir)
 		_, err = cmd.Output()
@@ -127,5 +162,50 @@ func main() {
 			log.Fatal(err)
 		}
 		fmt.Printf("Successfully wrote to : %s\n", dir)
+	}
+	if options["git"] {
+		cmd := exec.Command("git", "init")
+		cmd.Dir = name
+
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Fatalf("Failed to initialize git: %v\nOutput: %s", err, output)
+		}
+		if len(output) > 0 {
+			fmt.Printf("git init output: %s", output)
+		} else {
+			fmt.Println("✓ Initialized git repository")
+		}
+
+		fmt.Printf("Adding remote: %s\n", remote)
+		cmd = exec.Command("git", "remote", "add", "origin", remote)
+		cmd.Dir = name
+
+		output, err = cmd.CombinedOutput()
+		if err != nil {
+			if strings.Contains(string(output), "already exists") {
+				fmt.Println("✓ Remote 'origin' already exists")
+				fmt.Println("Replacing 'origin'...")
+			} else {
+				log.Fatalf("Failed to add remote: %v\nOutput: %s", err, output)
+			}
+		} else {
+			if len(output) > 0 {
+				fmt.Printf("Remote added: %s", output)
+			} else {
+				fmt.Println("✓ Added remote 'origin'")
+			}
+		}
+	}
+
+	if options["git-local"] {
+		cmd := exec.Command("git", "init")
+		cmd.Dir = name
+
+		output, err := cmd.Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Successfully initialized: %s\n", output)
 	}
 }
